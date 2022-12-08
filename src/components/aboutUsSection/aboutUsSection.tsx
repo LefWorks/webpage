5ee9ebaa4fd1014fd2182aa5c4c20452762f38d2
fedefunes96 <fede.funes96@gmail.com>
2022-12-07 21:27:39 -0300

    Cambio texto

--- src/components/aboutUsSection/aboutUsSection.tsx
+++ src/components/aboutUsSection/aboutUsSection.tsx
@@ -10,15 +10,13 @@ const AboutUsSection = () => {
           <Text variant="title">How are we different?</Text>
           <Text variant="paragraph">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry{"'"}s standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book.
-            <br />
-            <br />
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry{"'"}s standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book.
+            industry. Lorem Ipsum has been the industrys standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. Lorem Ipsum is simply
+            dummy text of the printing and typesetting industry. Lorem Ipsum has
+            been the industrys standard dummy text ever since the 1500s, when an
+            unknown printer took a galley of type and scrambled it to make a
+            type specimen book.
           </Text>
         </div>
 
